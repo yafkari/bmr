4741755ff5931f82af4a545ac80483462bc92ff5
yafkari <younesafkpro@gmail.com>
2019-10-07 17:14:55 +0200

    add textinputs for leftPane + add ActivityLevel enum

---
+++ src/bmr_afkari/ActivityLevel.java
@@ -0,0 +1,9 @@
+package bmr_afkari;
+
+/**
+ *
+ * @author Younes
+ */
+public enum ActivityLevel {
+    SEDENTARY, INACTIVE, ACTIVE, VERY_ACTIF, EXTREMELY_ACTIF;
+}
--- src/bmr_afkari/BMR_Afkari.java
+++ src/bmr_afkari/BMR_Afkari.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -13,6 +14,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -26,28 +30,38 @@ public class BMR_Afkari extends Application {
         primaryStage.setTitle("Basal Metabolic Rate");
 
         VBox root = new VBox(10);
-        root.setAlignment(Pos.CENTER);
+        root.setAlignment(Pos.TOP_CENTER);
         root.setPadding(new Insets(25, 25, 25, 25));
 
         HBox rootPanel = new HBox();
-        
+
         GridPane leftPanel = new GridPane();
         GridPane rightPanel = new GridPane();
         leftPanel.setVgap(20);
         rightPanel.setVgap(20);
 
         Button submitButton = new Button("Submit");
+        Text leftPaneTitle = new Text("Data");
+        leftPaneTitle.setUnderline(true);
+        leftPaneTitle.setFont(Font.font("Calibri", FontWeight.BOLD, 16));
+
         Label sizeLabel = new Label("Size (cm)");
+        TextField sizeInput = new TextField();
         Label weightLabel = new Label("Weight (kg)");
+        TextField weightInput = new TextField();
         Label ageLabel = new Label("Age (years)");
+        TextField ageInput = new TextField();
         Label genderLabel = new Label("Gender");
         Label lifestyleLabel = new Label("Life Style");
+        
 
-        leftPanel.add(sizeLabel, 0, 0);
-        leftPanel.add(weightLabel, 0, 1);
-        leftPanel.add(ageLabel, 0, 2);
-        leftPanel.add(genderLabel, 0, 3);
-        leftPanel.add(lifestyleLabel, 0, 4);
+        leftPanel.add(leftPaneTitle, 0, 0);
+        leftPanel.add(sizeLabel, 0, 1);
+        leftPanel.add(sizeInput, 1, 1);
+        leftPanel.add(weightLabel, 0, 2);
+        leftPanel.add(ageLabel, 0, 3);
+        leftPanel.add(genderLabel, 0, 4);
+        leftPanel.add(lifestyleLabel, 0, 5);
 
         rootPanel.getChildren().addAll(leftPanel, rightPanel);
         root.getChildren().addAll(rootPanel, submitButton);
