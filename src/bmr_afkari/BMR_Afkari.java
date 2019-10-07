bb434bb7790ad0f63f33f85deebbcc79f4a9a1f0
yafkari <younesafkpro@gmail.com>
2019-10-07 11:35:37 +0200

    add spacing

--- src/bmr_afkari/BMR_Afkari.java
+++ src/bmr_afkari/BMR_Afkari.java
@@ -6,9 +6,13 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -16,22 +20,27 @@ import javafx.stage.Stage;
  * @author 52196
  */
 public class BMR_Afkari extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Basal Metabolic Rate");
+
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
-        
+        root.setPadding(new Insets(25, 25, 25, 25));
+
         HBox rootPanel = new HBox();
+        
         GridPane leftPanel = new GridPane();
         GridPane rightPanel = new GridPane();
-        
+        leftPanel.setVgap(20);
+        rightPanel.setVgap(20);
+
         Button submitButton = new Button("Submit");
         Label sizeLabel = new Label("Size (cm)");
         Label weightLabel = new Label("Weight (kg)");
         Label ageLabel = new Label("Age (years)");
-        Label genderLabel = new Label("Life Style");
+        Label genderLabel = new Label("Gender");
         Label lifestyleLabel = new Label("Life Style");
 
         leftPanel.add(sizeLabel, 0, 0);
@@ -39,7 +48,7 @@ public class BMR_Afkari extends Application {
         leftPanel.add(ageLabel, 0, 2);
         leftPanel.add(genderLabel, 0, 3);
         leftPanel.add(lifestyleLabel, 0, 4);
-        
+
         rootPanel.getChildren().addAll(leftPanel, rightPanel);
         root.getChildren().addAll(rootPanel, submitButton);
         Scene scene = new Scene(root, 600, 400);
