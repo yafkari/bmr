9eda3f97f07602b295fccd83bd3d9793784afff9
yafkari <younesafkpro@gmail.com>
2019-10-07 11:27:21 +0200

    add labels

--- src/bmr_afkari/BMR_Afkari.java
+++ src/bmr_afkari/BMR_Afkari.java
@@ -1,9 +1,11 @@
 package bmr_afkari;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -14,20 +16,29 @@ import javafx.stage.Stage;
  * @author 52196
  */
 public class BMR_Afkari extends Application {
-
+    
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Basal Metabolic Rate");
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
-
-        Button submitButton = new Button("Submit");
-
+        
         HBox rootPanel = new HBox();
         GridPane leftPanel = new GridPane();
         GridPane rightPanel = new GridPane();
+        
+        Button submitButton = new Button("Submit");
+        Label sizeLabel = new Label("Size (cm)");
+        Label weightLabel = new Label("Weight (kg)");
+        Label ageLabel = new Label("Age (years)");
+        Label genderLabel = new Label("Life Style");
+        Label lifestyleLabel = new Label("Life Style");
 
-        // TODO
+        leftPanel.add(sizeLabel, 0, 0);
+        leftPanel.add(weightLabel, 0, 1);
+        leftPanel.add(ageLabel, 0, 2);
+        leftPanel.add(genderLabel, 0, 3);
+        leftPanel.add(lifestyleLabel, 0, 4);
         
         rootPanel.getChildren().addAll(leftPanel, rightPanel);
         root.getChildren().addAll(rootPanel, submitButton);
