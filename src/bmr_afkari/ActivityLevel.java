b810f72b9884d59f94a9c974e9d3bb2ae33d2bb2
yafkari <52196@etu.he2b.be>
2019-10-08 20:18:57 +0200

    add constructor to ActivityLevel enum

--- src/bmr_afkari/ActivityLevel.java
+++ src/bmr_afkari/ActivityLevel.java
@@ -5,5 +5,19 @@ package bmr_afkari;
  * @author Younes
  */
 public enum ActivityLevel {
-    SEDENTARY, INACTIVE, ACTIVE, VERY_ACTIF, EXTREMELY_ACTIF;
+    SEDENTARY("Sedentary"),
+    INACTIVE("Inactive"),
+    ACTIVE("Active"),
+    VERY_ACTIVE("Very active"),
+    EXTREMELY_ACTIVE("Extremely active");
+
+    private String label;
+
+    ActivityLevel(String label) {
+        this.label = label;
+    }
+
+    public String toString() {
+        return label;
+    }
 }
